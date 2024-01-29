12387b7aafa1f43c1f33217ac108e8f7432fbb4c
Nicole Stein <nicolethenerd@gmail.com>
2024-01-29 12:12:48 -0500

    Finish logic, add styling

--- src/App.js
+++ src/App.js
@@ -1,45 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { Button, IconButton, Input } from '@mui/material';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function AddItem({ onAddItem }) {
-  return (
-    <>
-      <input placeholder="Buy milk"></input>
-      <button onClick={onAddItem}>Add Item</button>
-    </>);
-}
+  const [itemName, setItemName] = useState('');
 
-function TodoItem({ itemName, onDeleteItem }) {
-  function DeleteButton() {
-    return <button onClick={() => onDeleteItem(this)}>X</button>;
+  function handleItemNameChange(e) {
+    setItemName(e.target.value);
   }
 
-  return (<li>{itemName} <DeleteButton /></li>);
+  function handleSubmit() {
+    onAddItem(itemName);
+  }
+
+  return (
+    <div className="AddItem">
+      <Input placeholder="Buy milk" onChange={handleItemNameChange} />
+      <Button onClick={handleSubmit}>Add Item</Button>
+    </div>);
+}
+
+function TodoItem({ todo, onDeleteItem }) {
+  function DeleteButton() {
+    return (
+      <IconButton color="inherit"
+                  className="DeleteButtton" 
+                  onClick={() => onDeleteItem(todo.id)} 
+                  aria-label="delete">
+        <DeleteOutlineOutlinedIcon />
+      </IconButton>);
+  }
+
+  return (<li key={todo.id} className="TodoItem">{todo.name}<DeleteButton /></li>);
+}
+
+let idCounter = 0;
+class Todo {
+
+  constructor(name) {
+    this.name = name;
+    this.id = idCounter++;
+  }
 }
 
 function App() {
-  const [todoItems, setTodoItems] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-  function handleDeleteItem(item) {
-    const updatedTodoItems = todoItems.filter((val) => {return val != item;});
-    setTodoItems(updatedTodoItems);
+  function handleDeleteItem(id) {
+    const updatedTodos = todos.filter((todo) => {return id !== todo.id;});
+    setTodos(updatedTodos);
   }
 
   function handleAddItem(itemName) {
-    const updatedTodoItems = todoItems.slice();
-    updatedTodoItems.push(<TodoItem itemName={itemName} onDeleteItem={(item) => handleDeleteItem(item)} />);
-    setTodoItems(updatedTodoItems);
+    const updatedTodos = todos.slice();
+    updatedTodos.push(new Todo(itemName));
+    setTodos(updatedTodos);
   }
 
+  const todoItems = todos.map((todo) => <TodoItem todo={todo} onDeleteItem={handleDeleteItem} />);
+
   return (
-    <div className="App">
-      <h1>TODO</h1>
-      <ul>
-        {todoItems}
-      </ul>
-      <AddItem onAddItem={handleAddItem} />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <h1>TODO</h1>
+        <ul>
+          {todoItems}
+        </ul>
+        <AddItem onAddItem={handleAddItem} />
+      </div>
+    </ThemeProvider>
   );
 }
 
