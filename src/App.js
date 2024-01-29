d6911df18b33af26a4b7a3f7a07cb852519ec6c2
Nicole Stein <nicolethenerd@gmail.com>
2024-01-28 20:07:43 -0500

    Begin todo app implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function AddItem({ onAddItem }) {
+  return (
+    <>
+      <input placeholder="Buy milk"></input>
+      <button onClick={onAddItem}>Add Item</button>
+    </>);
+}
+
+function TodoItem({ itemName, onDeleteItem }) {
+  function DeleteButton() {
+    return <button onClick={() => onDeleteItem(this)}>X</button>;
+  }
+
+  return (<li>{itemName} <DeleteButton /></li>);
+}
 
 function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  function handleDeleteItem(item) {
+    const updatedTodoItems = todoItems.filter((val) => {return val != item;});
+    setTodoItems(updatedTodoItems);
+  }
+
+  function handleAddItem(itemName) {
+    const updatedTodoItems = todoItems.slice();
+    updatedTodoItems.push(<TodoItem itemName={itemName} onDeleteItem={(item) => handleDeleteItem(item)} />);
+    setTodoItems(updatedTodoItems);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO</h1>
+      <ul>
+        {todoItems}
+      </ul>
+      <AddItem onAddItem={handleAddItem} />
     </div>
   );
 }
